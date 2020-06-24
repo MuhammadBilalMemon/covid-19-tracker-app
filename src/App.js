19a880a3d1607b175eac3ac4cc02e663174ab243
Muhammad Bilal <billu97.memon@gmail.com>
2020-06-24 02:10:21 -0700

    Updated Component Folder
    Added Api foler & file, Updated Component foler & file.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 
-import './App.css';
+import { Cards, Chart, CountryPicker } from './components';
+
+import styles from './App.module.css'
 
 function App() {
   return (
-    <div className="App">
-      <h1>Covid 19 Tracker App</h1>
-      <p>By Muhammad Bilal</p>
+    <div className={styles.container}>
+      <Cards />
+      <CountryPicker />
+      <Chart />
     </div>
   );
 }
